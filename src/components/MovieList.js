8d3419b0c6e70f957bb8c662f8d8b8c028fcd01d
brahiiim <56367267+brahiiim@users.noreply.github.com>
2021-01-23 08:02:25 +0100

    Create MovieList component
    Create basic display of the MovieList component
    Create Styled.js to contain sub-components with styled-component (No CSS yet)

---
+++ src/components/MovieList.js
@@ -0,0 +1,9 @@
+import { MovieListTitle, PageWrapper } from "../components/Styled";
+
+export default function MovieList() {
+  return (
+    <PageWrapper>
+      <MovieListTitle>Movie Catalogue</MovieListTitle>
+    </PageWrapper>
+  );
+}
---
+++ src/components/Styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const PageWrapper = styled.div``;
+
+const MovieListTitle = styled.h1``;
+
+export { PageWrapper, MovieListTitle };
