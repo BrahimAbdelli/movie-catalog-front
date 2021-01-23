f0b59b82ab4c3f175bce358590e0035ea479f425
brahiiim <56367267+brahiiim@users.noreply.github.com>
2021-01-23 11:01:48 +0100

    Updating Movie Details Component
    Updating the Movie Details component to work dynamically with the backend and deleting the previous genre query.

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -14,44 +14,24 @@ import { queryApi } from "../utils/queryApi";
 
 export default function MovieDetails(props) {
   const dispatch = useDispatch();
-  const [genre, setGenre] = useState([]);
   function unselect() {
     dispatch(unselectMovie(props.movie));
   }
-
-  useEffect(() => {
-    async function fetch() {
-      const [res] = await queryApi(
-        "genre/movie/list?api_key=efb3d7bd52872c1d07a1787a51d66394"
-      );
-      setGenre(res.genres);
-    }
-    fetch();
-  }, [props]);
 
   return (
     <PageWrapperDetail>
       <MovieTitle>{props.movie?.title}</MovieTitle>
       <MovieInfoWrapper>
-        <MovieImage
-          src={
-            props.movie?.poster_path
-              ? `http://image.tmdb.org/t/p/w185${props.movie?.poster_path}`
-              : `https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`
-          }
-        />
-        <MovieInfo>Release Date: {props.movie?.release_date}</MovieInfo>
-        <MovieInfo>Rating:{props.movie?.vote_average}</MovieInfo>
-        <MovieInfo>Genres:</MovieInfo>
-        <GenreList>
-          {genre
-            ?.filter(function (gen) {
-              return props.movie.genre_ids.indexOf(gen.id) > -1;
-            })
-            .map((gen, index) => (
-              <li key={index}>{gen.name}</li>
-            ))}
-        </GenreList>
+      <MovieImage
+        src={
+          props.movie?.image
+            ? process.env.REACT_APP_API_URL_UPLOADS + props.movie?.image
+            : `https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`
+        }
+      />
+        <MovieInfo>Release Date: {props.movie?.releaseDate}</MovieInfo>
+        <MovieInfo>Rating:{props.movie?.rating}</MovieInfo>
+        <MovieInfo>Genres:{props.movie?.genre}</MovieInfo>
         <ReturnButton onClick={unselect}>Back to search results</ReturnButton>
       </MovieInfoWrapper>
     </PageWrapperDetail>
