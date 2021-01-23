e98633cc2d95ae5036bf115802c9f737768f329b
brahiiim <56367267+brahiiim@users.noreply.github.com>
2021-01-23 08:08:20 +0100

    Add style to SearchBar component
    Style SearchBar component
    -With responsivness

--- src/components/Styled.js
+++ src/components/Styled.js
@@ -4,11 +4,40 @@ const PageWrapper = styled.div``;
 
 const MovieListTitle = styled.h1``;
 
-const SearchBox = styled.div``;
+const SearchBox = styled.div`
+  margin: 0 20px;
+`;
 
-const SearchBoxInput = styled.input``;
+const SearchBoxInput = styled.input`
+  width: 70%;
+  border-radius: 5px;
+  min-height: 25px;
+  border: solid 2px #c62828;
+  background-color: #f5f5f5;
+  ::placeholder {
+    color: #c62828a3;
+    padding-left: 7.5px;
+  }
 
-const SearchBoxButton = styled.button``;
+  @media only screen and (max-width: 768px) {
+    width: 60%;
+  }
+`;
+
+const SearchBoxButton = styled.button`
+  width: 15%;
+  margin-left: 5%;
+  border-radius: 5px;
+  min-height: 31px;
+  font-weight: bold;
+  background-color: #f5f5f5;
+  color: #c62828;
+  border: solid 2px #c62828;
+
+  @media only screen and (max-width: 768px) {
+    width: 20%;
+  }
+`;
 
 const MoviesListWrapper = styled.div``;
 
